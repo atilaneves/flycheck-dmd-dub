41a839e18eb7159175c59a2f8b2f5f283191e33f
Atila Neves <atila.neves@gmail.com>
2018-02-08 17:55:59 +0100

    Update version

--- flycheck-dmd-dub.el
+++ flycheck-dmd-dub.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014 Atila Neves
 
 ;; Author:  Atila Neves <atila.neves@gmail.com>
-;; Version: 0.11
+;; Version: 0.12
 ;; Package-Requires: ((flycheck "0.24") (f "0.18.2"))
 ;; Keywords: languages
 ;; URL: http://github.com/atilaneves/flycheck-dmd-dub
